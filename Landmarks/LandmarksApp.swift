7a32def39ed06d30873837c7d9a9780e15cb4da6
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-10-29 13:01:17 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Bilal Dallali on 29/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
