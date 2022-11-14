23c866cce1ba8cbc532ed6786a8c7f723b1de6b7
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-11-14 22:26:43 +0100

    detail view

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Bilal Dallali on 05/11/2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
